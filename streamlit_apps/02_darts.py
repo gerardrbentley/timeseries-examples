667eaa12df13215cf8b163f4733fa66b1ad81123
Gerard Bentley <gerardrbentley@gmail.com>
2022-03-08 15:46:56 -0800

    chore: cleanup

--- streamlit_apps/02_darts.py
+++ streamlit_apps/02_darts.py
@@ -1,19 +1,25 @@
+from inspect import isclass
 import streamlit as st
+st.set_page_config(page_title='Darts + Streamlit', page_icon=":dart:", layout='wide')
 
 """
 # Streamlit + Darts
 
 Exploring the "Time Series Made Easy in Python" library [Darts](https://unit8co.github.io/darts/).
 Adding interactive web elements with [Streamlit](https://streamlit.io)
-
+"""
+with st.expander("What is this?"):
+    """\
+A Demo blending Darts and Streamlit, made with :heart: from [Gar's Bar](https://tech.gerardbentley.com/)
+"""
+"""
 ## Install 
 
 You can install darts using pip or conda
 
 ```sh
-# pip
 pip install darts
-# conda (recommended for dependencies)
+# OR (recommended for dependencies)
 conda install -c conda-forge -c pytorch u8darts-all
 ```
 """
@@ -24,14 +30,14 @@ Install it without all of the models with pip (from [docs](https://unit8co.githu
 # Install core only (without neural networks, Prophet or AutoARIMA): 
 pip install u8darts
 
-# Install core + neural networks (PyTorch): pip install 
-u8darts[torch]
+# Install core + neural networks (PyTorch): 
+pip install u8darts[torch]
 
-# Install core + Facebook Prophet: pip install 
-u8darts[prophet]
+# Install core + Facebook Prophet: 
+pip install u8darts[prophet]
 
-# Install core + AutoARIMA: pip install 
-u8darts[pmdarima]
+# Install core + AutoARIMA: 
+pip install u8darts[pmdarima]
 ```
 
 Install it without all of the models with conda:
@@ -66,8 +72,8 @@ with st.expander("Train Time Series"):
     attributes = [x for x in dir(train) if not x.startswith("_")]
     st.write("Non-private Attributes:", attributes)
 
-with st.expander("Xarray View"):
-    st.markdown(train, unsafe_allow_html=True)
+with st.expander("Dataframe View"):
+    st.dataframe(series.pd_dataframe())
 
 "## Fit an exponential smoothing model, and make a (probabilistic) prediction over the validation series' duration:"
 
@@ -101,11 +107,11 @@ with st.echo('below'):
     series.plot()
 
     st.subheader("Training Controls")
-    num_periods = st.number_input("Number of validation months", min_value=0, max_value=len(series), value=36, help='How many months worth of datapoints to exclude from training')
+    num_periods = st.slider("Number of validation months", min_value=2, max_value=len(series) - 24, value=36, help='How many months worth of datapoints to exclude from training')
     num_samples = st.number_input("Number of prediction samples", min_value=1, max_value=10000, value=1000, help="Number of times a prediction is sampled for a probabilistic model")
     st.subheader("Plotting Controls")
-    low_quantile = st.number_input('Lower Percentile', min_value=0.01, max_value=0.99, value=0.05, help='The quantile to use for the lower bound of the plotted confidence interval.')
-    high_quantile = st.number_input('High Percentile', min_value=0.01, max_value=0.99, value=0.95, help='The quantile to use for the upper bound of the plotted confidence interval.')
+    low_quantile = st.slider('Lower Percentile', min_value=0.01, max_value=0.99, value=0.05, help='The quantile to use for the lower bound of the plotted confidence interval.')
+    high_quantile = st.slider('High Percentile', min_value=0.01, max_value=0.99, value=0.95, help='The quantile to use for the upper bound of the plotted confidence interval.')
     
     train, val = series[:-num_periods], series[-num_periods:]
     model = ExponentialSmoothing()
@@ -116,58 +122,79 @@ with st.echo('below'):
     plt.legend()
     st.pyplot(interactive_fig)
 
-pandas_options = pd.read_csv('pandas_frequencies.csv')
-pandas_options = pandas_options.set_index('Date Offset')
-options = {'Weekly': ('W', 52), 'Monthly': ('M', 12), 'Yearly': ('A', 1)}
 """## Go Wild!\
 
 Use your own csv data that has a well formed time series and plot some forecasts!
 
-(Limited to ExponentialSmoothing. For now...)
+Or use one of the example Darts [datasets](https://github.com/unit8co/darts/tree/master/datasets)
+
+(Limited to ExponentialSmoothing with single variable. Resampling time period will only perform summation. For now...)
 """
+
+import darts.datasets as ds
+all_datasets = {x: ds.__getattribute__(x) for x in dir(ds) if isclass(ds.__getattribute__(x)) and x not in ("DatasetLoaderMetadata", "DatasetLoaderCSV")}
+with st.expander("More info on Darts Datasets"):
+    for name, dataset in all_datasets.items():
+        st.write(f"#### {name}\n\n{dataset.__doc__}")
+
 with st.echo('below'):
-    data = st.file_uploader("New Timeseries csv")
-    delimiter = st.text_input("CSV Delimiter", value=',', max_chars=1, help='How your CSV values are separated')
-    if data is not None:
-        custom_df = pd.read_csv(data, sep=delimiter)
-        with st.expander("Show Raw Data"):
-            st.dataframe(custom_df)
+    use_example = st.checkbox("Use example dataset")
+    if use_example:
+        dataset_choice = st.selectbox("Example Dart Dataset", all_datasets, index=5)
+        with st.spinner("Fetching Dataset"):
+            dataset = all_datasets[dataset_choice]()
+            timeseries = dataset.load()
+            custom_df = timeseries.pd_dataframe()
+            custom_df['Period'] = custom_df.index.to_series()
+            custom_df = custom_df[['Period', *custom_df.columns[:-1]]]
+    else:
+        csv_data = st.file_uploader("New Timeseries csv")
+        delimiter = st.text_input("CSV Delimiter", value=',', max_chars=1, help='How your CSV values are separated')
 
-        columns = list(custom_df.columns)
-        with st.expander("Show all columns"):
-            st.write(' | '.join(columns))
+        if csv_data is None:
+            st.warning("Upload a CSV to analyze")
+            st.stop()
 
-        time_col = st.selectbox("Time Column", columns, help="Name of the column in your csv with time period data")
-        value_cols = st.multiselect("Values Column(s)", columns, columns[1], help="Name of column(s) with values to sample and forecast")
-        sampling_period = st.selectbox("Time Series Period", options, help='How to define samples. Pandas will sum entries between periods to create a well-formed Time Series')
+        custom_df = pd.read_csv(csv_data, sep=delimiter)
+    with st.expander("Show Raw Data"):
+        st.dataframe(custom_df)
 
-        custom_df[time_col] = pd.to_datetime(custom_df[time_col])
-        freq_string, periods_per_year = options[sampling_period]
-        custom_df = custom_df.set_index(time_col).resample(freq_string).sum()
-        with st.expander("Show Resampled Data"):
-            st.write("Number of samples:", len(custom_df))
-            st.dataframe(custom_df)
+    columns = list(custom_df.columns)
+    with st.expander("Show all columns"):
+        st.write(' | '.join(columns))
 
-        custom_series = TimeSeries.from_dataframe(custom_df, value_cols=value_cols)
+    time_col = st.selectbox("Time Column", columns, help="Name of the column in your csv with time period data")
+    value_cols = st.selectbox("Values Column(s)", columns, 1, help="Name of column(s) with values to sample and forecast")
+    options = {'Monthly': ('M', 12), 'Weekly': ('W', 52), 'Yearly': ('A', 1), 'Daily':  ('D', 365), 'Hourly': ('H', 365 * 24), 'Quarterly': ('Q', 8)}
+    sampling_period = st.selectbox("Time Series Period", options, help='How to define samples. Pandas will sum entries between periods to create a well-formed Time Series')
 
-        st.subheader("Custom Training Controls")
-        max_periods = len(custom_df) - (2 * periods_per_year)
-        num_periods = st.number_input("Number of validation periods", key='cust_period', min_value=2, max_value=max_periods, value=max_periods, help='How many periods worth of datapoints to exclude from training')
-        num_samples = st.number_input("Number of prediction samples", key='cust_sample', min_value=1, max_value=10000, value=1000, help="Number of times a prediction is sampled for a probabilistic model")
-        
-        st.subheader("Custom Plotting Controls")
-        low_quantile = st.number_input('Lower Percentile', key='cust_low', min_value=0.01, max_value=0.99, value=0.05, help='The quantile to use for the lower bound of the plotted confidence interval.')
-        high_quantile = st.number_input('High Percentile', key='cust_high', min_value=0.01, max_value=0.99, value=0.95, help='The quantile to use for the upper bound of the plotted confidence interval.')
+    custom_df[time_col] = pd.to_datetime(custom_df[time_col])
+    freq_string, periods_per_year = options[sampling_period]
+    custom_df = custom_df.set_index(time_col).resample(freq_string).sum()
+    with st.expander("Show Resampled Data"):
+        st.write("Number of samples:", len(custom_df))
+        st.dataframe(custom_df)
 
-        train, val = custom_series[:-num_periods], custom_series[-num_periods:]
-        model = ExponentialSmoothing()
-        model.fit(train)
-        prediction = model.predict(len(val), num_samples=num_samples)
+    custom_series = TimeSeries.from_dataframe(custom_df, value_cols=value_cols)
+    st.subheader("Custom Training Controls")
+    max_periods = len(custom_series) - (2 * periods_per_year)
+    default_periods = min(10, max_periods)
+    num_periods = st.slider("Number of validation periods", key='cust_period', min_value=2, max_value=max_periods, value=default_periods, help='How many periods worth of datapoints to exclude from training')
+    num_samples = st.number_input("Number of prediction samples", key='cust_sample', min_value=1, max_value=10000, value=1000, help="Number of times a prediction is sampled for a probabilistic model")
+    
+    st.subheader("Custom Plotting Controls")
+    low_quantile = st.slider('Lower Percentile', key='cust_low', min_value=0.01, max_value=0.99, value=0.05, help='The quantile to use for the lower bound of the plotted confidence interval.')
+    high_quantile = st.slider('High Percentile', key='cust_high', min_value=0.01, max_value=0.99, value=0.95, help='The quantile to use for the upper bound of the plotted confidence interval.')
 
-        custom_fig = plt.figure()
-        custom_series.plot()
+    train, val = custom_series[:-num_periods], custom_series[-num_periods:]
+    model = ExponentialSmoothing()
+    model.fit(train)
+    prediction = model.predict(len(val), num_samples=num_samples)
 
-        prediction.plot(label='forecast', low_quantile=low_quantile, high_quantile=high_quantile)
+    custom_fig = plt.figure()
+    custom_series.plot()
 
-        plt.legend()
-        st.pyplot(custom_fig)
+    prediction.plot(label='forecast', low_quantile=low_quantile, high_quantile=high_quantile)
+
+    plt.legend()
+    st.pyplot(custom_fig)
